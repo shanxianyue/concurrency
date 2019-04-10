baee908451ccf982852a266f5a0232a8a0c8dfb1
xupeiji <279149639@qq.com>
2019-04-10 16:40:16 +0800

    自定义一个简单的线程池2 增加拒绝策略和shutdown方法

--- thread-basis/src/main/java/com/xpj/threadpool/SimpleThreadPool.java
+++ thread-basis/src/main/java/com/xpj/threadpool/SimpleThreadPool.java
@@ -1,6 +1,8 @@
 package com.xpj.threadpool;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.IntStream;
@@ -12,25 +14,57 @@ import java.util.stream.IntStream;
  * @Date: Created in 2019/4/10 10:21
  */
 public class SimpleThreadPool {
-    private final int size;
-    private final static int DEFAULT_SIZE = 10;
 
+    /**线程池大小*/
+    private final int size;
+
+    /**自增 用于线程名称命名*/
     private volatile int seq = 0;
 
+    /**任务队列最大可接受队列个数*/
+    private final int taskQueueSize;
+
+    /**是否已经关闭线程池*/
+    private volatile boolean isShutdown = false;
+
+    /**拒绝策略*/
+    private DiscardPolicy discardPolicy;
+
+    /**默认线程池大小*/
+    private final static int DEFAULT_SIZE = 10;
+
+    /**默认任务队列大小*/
+    private static final int DEFAULT_TASK_QUEUE_SIZE = 2000;
+
+    /**线程名称前缀*/
     private static final String THREAD_PREFIX = "SIMPLE_THREAD_POOL_";
 
+    /**默认线程组*/
     private static final ThreadGroup GROUP = new ThreadGroup("POOL_GROUP");
 
+    /**任务队列*/
     private static final LinkedList<Runnable> TASK_QUEUE = new LinkedList<>();
 
-//    private static final List<WorkerThread> THREAD_QUEUE = new ArrayList<>();
+    /**线程池线程队列*/
+    private static final List<WorkerThread> THREAD_QUEUE = new ArrayList<>();
+
+    /**默认拒绝策略*/
+    private static final DiscardPolicy DEFAULT_DISCARD_POLICY = () -> {
+        throw new DiscardException("discard the task");
+    };
 
     public SimpleThreadPool() {
-        this(DEFAULT_SIZE);
+        this(DEFAULT_SIZE, DEFAULT_TASK_QUEUE_SIZE, DEFAULT_DISCARD_POLICY);
     }
 
-    public SimpleThreadPool(int size) {
+    public SimpleThreadPool(int size, int taskQueueSize) {
+        this(size, taskQueueSize, DEFAULT_DISCARD_POLICY);
+    }
+
+    public SimpleThreadPool(int size, int taskQueueSize, DiscardPolicy discardPolicy) {
         this.size = size;
+        this.taskQueueSize = taskQueueSize;
+        this.discardPolicy = discardPolicy;
         init();
     }
 
@@ -44,15 +78,49 @@ public class SimpleThreadPool {
         String name = THREAD_PREFIX + (++seq);
         WorkerThread task = new WorkerThread(GROUP, name);
         task.start();
-        Optional.of(name + " started").ifPresent(System.out::println);
-//        THREAD_QUEUE.add(task);
+        THREAD_QUEUE.add(task);
     }
 
     public void submit(Runnable runnable){
+        if (isShutdown){
+            throw new IllegalStateException("threadpool already shutdown ,cann't submit task!");
+        }
         synchronized (TASK_QUEUE) {
+            if (TASK_QUEUE.size() >= taskQueueSize){
+                discardPolicy.discard();
+            }
             TASK_QUEUE.addLast(runnable);
             TASK_QUEUE.notifyAll();
         }
+    }
+
+    public void shutdown() throws InterruptedException {
+        isShutdown = true;
+        while(!TASK_QUEUE.isEmpty()){
+            TimeUnit.MILLISECONDS.sleep(50);
+        }
+        int threadQueueSize = THREAD_QUEUE.size();
+        while(threadQueueSize > 0){
+            for (WorkerThread workerThread : THREAD_QUEUE) {
+                if (workerThread.taskState == TaskState.BLOCKED){
+                    workerThread.interrupt();
+                    workerThread.close();
+                    threadQueueSize--;
+                }else{
+                    TimeUnit.MILLISECONDS.sleep(50);
+                }
+            }
+        }
+    }
+
+    public static class DiscardException extends RuntimeException{
+        public DiscardException(String message) {
+            super(message);
+        }
+    }
+
+    public interface DiscardPolicy{
+        void discard() throws DiscardException;
     }
 
     private enum TaskState{
@@ -99,14 +167,14 @@ public class SimpleThreadPool {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SimpleThreadPool threadPool = new SimpleThreadPool();
         IntStream.range(0, 40).forEach(i ->
             threadPool.submit(() -> {
                 Optional.of("the runnable " + i + " is serviced by " + Thread.currentThread().getName() + " start")
                         .ifPresent(System.out::println);
                 try {
-                    TimeUnit.SECONDS.sleep(10);
+                    TimeUnit.SECONDS.sleep(1);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -114,5 +182,11 @@ public class SimpleThreadPool {
                         .ifPresent(System.out::println);
             })
         );
+        threadPool.shutdown();
+        System.out.println("--------------main-------------------");
+        TimeUnit.MILLISECONDS.sleep(50);
+        threadPool.submit(() -> {
+
+        });
     }
 }
